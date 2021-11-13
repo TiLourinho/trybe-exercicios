d119cc3c9d5675af3f5d8bbc18359e68e4c5d373
TiLourinho <lourinho.tiago@gmail.com>
2021-11-13 15:44:09 -0300

    Exercício 3

--- 01. Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 7.3 - JavaScript ES6 - Primeiros passos em Jest/Exercicios/sum.js
+++ 01. Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 7.3 - JavaScript ES6 - Primeiros passos em Jest/Exercicios/sum.js
@@ -27,9 +27,40 @@ describe('Tests: myRemove function', () => {
 
   test('Check if myRemove([1, 2, 3, 4], 3) does not return [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-  })
+  });
 
   test('Check if myRemove([1, 2, 3, 4], 5) returns the expected array', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-  })
+  });
+});
+
+// 3.
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+};
+
+describe('Tests: myFizzBuzz function', () => {
+  test('Check if myFizzBuzz(15) gets the expected return', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  });
+
+  test('Check if myFizzBuzz(9) gets the expected return', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+  });
+
+  test('Check if myFizzBuzz(10) gets the expected return', () => {
+    expect(myFizzBuzz(10)).toBe('buzz');
+  });
+
+  test('Check if myFizzBuzz(7) gets the expected return', () => {
+    expect(myFizzBuzz(7)).toBe(7);
+  });
+
+  test('Check if myFizzBuzz("15") gets the expected return', () => {
+    expect(myFizzBuzz("15")).toBe(false);
+  });
 });
