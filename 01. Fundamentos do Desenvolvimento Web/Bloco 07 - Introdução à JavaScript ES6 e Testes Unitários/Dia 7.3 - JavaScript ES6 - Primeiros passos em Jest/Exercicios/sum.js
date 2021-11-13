5a88b79edd52db47df5b46f138e65376f9159676
TiLourinho <lourinho.tiago@gmail.com>
2021-11-13 15:03:17 -0300

    Exercício 2

--- 01. Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 7.3 - JavaScript ES6 - Primeiros passos em Jest/Exercicios/sum.js
+++ 01. Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 7.3 - JavaScript ES6 - Primeiros passos em Jest/Exercicios/sum.js
@@ -1,9 +1,35 @@
+// 1.
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
   }
 
   return a + b;
-}
+};
 
 module.exports = sum;
+
+// 2.
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+};
+
+describe('Tests: myRemove function', () => {
+  test('Check if myRemove([1, 2, 3, 4], 3) returns the expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  test('Check if myRemove([1, 2, 3, 4], 3) does not return [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+
+  test('Check if myRemove([1, 2, 3, 4], 5) returns the expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
+});
