91167b19862f83bed5838570a3ca60d9a4a3e112
TiLourinho <lourinho.tiago@gmail.com>
2022-01-12 09:43:21 -0300

    Ajustes

---
+++ 02. Desenvolvimento Front-end/Bloco 12 - Ciclo de Vida de Componentes e React Router/Dia 12.1 - Ciclo de vida de componentes/exemplos/my-fixation-exercise/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PersonDetails from './components/PersonDetails';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <PersonDetails />
+    );
+  }
+}
+
+export default App;
