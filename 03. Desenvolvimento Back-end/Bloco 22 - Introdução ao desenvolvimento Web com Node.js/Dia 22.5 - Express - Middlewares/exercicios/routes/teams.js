74a2775e84fb847694205e4c933d84e424478d54
TiLourinho <lourinho.tiago@gmail.com>
2022-04-21 17:35:19 -0300

    Alterações

--- 03. Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 22.5 - Express - Middlewares/exercicios/routes/teams.js
+++ 03. Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 22.5 - Express - Middlewares/exercicios/routes/teams.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs').promises;
-const path = require('path');
 const teamsRouter = express.Router();
 const validateName = require('../middlewares/validateName');
 const validateInitials = require('../middlewares/validateInitials');
@@ -12,9 +11,13 @@ teamsRouter.post('/', validateName, validateInitials, validateCountry, async (re
   const { name, initials, country, league } = req.body;
   const toWrite = { team: name, initials: initials.toUpperCase(), country: country, league: league };
 
-  const data = await fs.readFile(filePath, 'utf-8') || [];
-  const result = JSON.parse(data);
+  const data = await fs.readFile(filePath, 'utf-8');
 
+  if (!data) {
+    await fs.writeFile(filePath, JSON.stringify([toWrite]));
+  }
+
+  const result = JSON.parse(data);
   result.push(toWrite);
   
   await fs.writeFile(filePath, JSON.stringify(result));
@@ -22,7 +25,7 @@ teamsRouter.post('/', validateName, validateInitials, validateCountry, async (re
   return res.status(OK).json({ team: name, initials: initials.toUpperCase(), country, league });
 });
 
-teamsRouter.get('/', async (req, res) => {
+teamsRouter.get('/', async (_req, res) => {
   const data = await fs.readFile('./teams.json', 'utf-8');
   const result = JSON.parse(data);
 
