f3afe8485a62529bcacab1ba225bf6534c21f05f
TiLourinho <lourinho.tiago@gmail.com>
2022-05-03 14:59:18 -0300

    Antes das validações

---
+++ 03. Desenvolvimento Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/Dia 23.2 - Arquitetura de Software - Camada de Controller e Service/exemplos/hello-msc/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Author = require('./controllers/Authors');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/authors', Author.getAll);
+app.get('/authors/:id', Author.findById);
+app.post('/authors', Author.createAuthor);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
