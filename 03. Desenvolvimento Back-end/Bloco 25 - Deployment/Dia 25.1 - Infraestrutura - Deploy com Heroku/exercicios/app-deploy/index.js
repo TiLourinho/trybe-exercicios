6e8af481812e456df988186393e67b3dd6aecb85
TiLourinho <lourinho.tiago@gmail.com>
2022-06-01 12:35:57 -0300

    Heroku noob

---
+++ 03. Desenvolvimento Back-end/Bloco 25 - Deployment/Dia 25.1 - Infraestrutura - Deploy com Heroku/exercicios/app-deploy/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Está vivo!!!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`);
+});
