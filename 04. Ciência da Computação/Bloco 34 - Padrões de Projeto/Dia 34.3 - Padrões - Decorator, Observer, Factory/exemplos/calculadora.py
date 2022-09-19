7af7807edc8b57e28d357c75f151a087f154b6ee
TiLourinho <lourinho.tiago@gmail.com>
2022-09-19 19:04:49 -0300

    refactor: numbers variables

--- 04. Ciência da Computação/Bloco 34 - Padrões de Projeto/Dia 34.3 - Padrões - Decorator, Observer, Factory/exemplos/calculadora.py
+++ 04. Ciência da Computação/Bloco 34 - Padrões de Projeto/Dia 34.3 - Padrões - Decorator, Observer, Factory/exemplos/calculadora.py
@@ -1,19 +1,28 @@
+NUMBERS_PT = {
+    "um": 1, "dois": 2, "três": 3, "quatro": 4, "cinco": 5,
+    "seis": 6, "sete": 7, "oito": 8, "nove": 9, "dez": 10,
+}
+NUMBERS_EN = {
+    "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+    "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10,
+}
+
+
 class Calculadora:
     def soma(self, x, y):
         return x + y
 
 
-class CalculadoraDecorada:
+class CalculadoraBase:
     def __init__(self, calculadora):
         self.calculadora = calculadora
 
+
+class CalculadoraDecorada(CalculadoraBase):
     def converter_numero(self, numero):
         if not isinstance(numero, str):
             return numero
-        return {
-            "um": 1, "dois": 2, "três": 3, "quatro": 4, "cinco": 5,
-            "seis": 6, "sete": 7, "oito": 8, "nove": 9, "dez": 10,
-        }.get(numero)
+        return NUMBERS_PT.get(numero)
 
     def soma(self, x, y):
         return self.calculadora.soma(
@@ -21,21 +30,15 @@ class CalculadoraDecorada:
         )
 
 
-class DecoratedCalculator:
-    def __init__(self, calculator):
-        self.calculator = calculator
-
-    def convert_number(self, number):
-        if not isinstance(number, str):
-            return number
-        return {
-            "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-            "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10,
-        }.get(number)
+class CalculadoraDecoradaIngles(CalculadoraBase):
+    def converter_numero(self, numero):
+        if not isinstance(numero, str):
+            return numero
+        return NUMBERS_EN.get(numero)
 
     def soma(self, x, y):
-        return self.calculator.soma(
-            self.convert_number(x), self.convert_number(y)
+        return self.calculadora.soma(
+            self.converter_numero(x), self.converter_numero(y)
         )
 
 
@@ -48,6 +51,6 @@ if __name__ == "__main__":
     teste = calculadora_decorada.soma('oito', 'dois')
     print(f'Calculadora Decorada: "oito" + "dois" = {teste}')
 
-    decorated_calculator = DecoratedCalculator(calculadora)
-    test = decorated_calculator.soma('six', 'four')
-    print(f'Decorated Calculator: "six" + "four" = {test}')
+    calculadora_decorada_ingles = CalculadoraDecoradaIngles(calculadora)
+    teste = calculadora_decorada_ingles.soma('six', 'four')
+    print(f'Decorated Calculator: "six" + "four" = {teste}')
