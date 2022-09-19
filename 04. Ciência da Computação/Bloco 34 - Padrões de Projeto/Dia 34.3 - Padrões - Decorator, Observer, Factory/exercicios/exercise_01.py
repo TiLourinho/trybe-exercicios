3e60f028135aae3a3bd6d13646133bce21203a53
TiLourinho <lourinho.tiago@gmail.com>
2022-09-19 20:10:02 -0300

    refactor: dynamism

--- 04. Ciência da Computação/Bloco 34 - Padrões de Projeto/Dia 34.3 - Padrões - Decorator, Observer, Factory/exercicios/exercise_01.py
+++ 04. Ciência da Computação/Bloco 34 - Padrões de Projeto/Dia 34.3 - Padrões - Decorator, Observer, Factory/exercicios/exercise_01.py
@@ -10,7 +10,7 @@ RESET = "\033[0m"
 
 
 class Log:
-    def dispara_log(message):
+    def dispara_log(self, message):
         return message
 
 
@@ -20,28 +20,29 @@ class BaseLog:
 
 
 class LogError(BaseLog):
-    def dispara(self):
-        message = f'{VERMELHO}O sistema está com erros{RESET}'
-        return self.log.dispara_log(message)
+    def dispara_log(self, message):
+        phrase = f'{VERMELHO}{self.log.dispara_log(message)}{RESET}'
+        return phrase
 
 
 class LogWarning(BaseLog):
-    def dispara(self):
-        message = f'{LARANJA}O sistema está lento{RESET}'
-        return self.log.dispara_log(message)
+    def dispara_log(self, message):
+        phrase = f'{LARANJA}{self.log.dispara_log(message)}{RESET}'
+        return phrase
 
 
 class LogSuccess(BaseLog):
-    def dispara(self):
-        message = f'{VERDE}O sistema está funcionando{RESET}'
-        return self.log.dispara_log(message)
+    def dispara_log(self, message):
+        phrase = f'{VERDE}{self.log.dispara_log(message)}{RESET}'
+        return phrase
 
 
-log_error = LogError(Log)
-print(log_error.dispara())
+log = Log()
+log_error = LogError(log)
+print(log_error.dispara_log('O sistema está com erros'))
 
-log_warning = LogWarning(Log)
-print(log_warning.dispara())
+log_warning = LogWarning(log)
+print(log_warning.dispara_log('O sistema está lento'))
 
-log_success = LogSuccess(Log)
-print(log_success.dispara())
+log_success = LogSuccess(log)
+print(log_success.dispara_log('O sistema está funcionando'))
